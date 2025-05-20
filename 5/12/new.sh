8952494bf7bdf5e852025721d397301e6eb474d2
Akhil K <98240442+KAkhil04@users.noreply.github.com>
2025-05-20 16:02:43 -0500

    Create new.sh

---
+++ 5/12/new.sh
@@ -0,0 +1,47 @@
+printf "\n"
+printf "%-1s %-38s %-1s %-28s %-1s %-28s %-1s %-28s %-1s %-28s %-1s %-28s %-1s %-28s %-1s\n" "|" "NODE" "|" "ALLOCATABLE CPU" "|" "CAPACITY CPU" "|" "CPU REQUESTS" "|" "CPU LIMITS" "|" "AVAILABLE CPU" "|" "ALLOCATABLE MEMORY" "|" "CAPACITY MEMORY" "|" "MEMORY REQUESTS" "|" "MEMORY LIMITS" "|" "AVAILABLE MEMORY" "|"
+printf -- "|----------------------------------------|------------------------------|------------------------------|------------------------------|------------------------------|------------------------------|------------------------------|------------------------------|------------------------------|------------------------------|------------------------------|\n"
+
+for node in $(oc get nodes -o name); do
+  node_name=$(echo "$node" | cut -d'/' -f2)
+  
+  # Fetch CPU details
+  allocatable_cpu=$(oc get node "$node_name" -o jsonpath='{.status.allocatable.cpu}')
+  capacity_cpu=$(oc get node "$node_name" -o jsonpath='{.status.capacity.cpu}')
+  cpu_line=$(oc describe node "$node_name" | awk '/Allocated resources:/,/Events:/' | grep '^  cpu')
+  cpu_requests=$(echo "$cpu_line" | awk '{print $2}')
+  cpu_limits=$(echo "$cpu_line" | awk '{print $4}')
+
+  if [[ "$allocatable_cpu" == *m ]]; then
+    alloc_cpu_m=${allocatable_cpu%m}
+  else
+    alloc_cpu_m=$((allocatable_cpu * 1000))
+  fi
+  req_cpu_m=${cpu_requests%m}
+  diff_cpu_m=$((alloc_cpu_m - req_cpu_m))
+  diff_cpu="${diff_cpu_m}m"
+
+  # Fetch Memory details
+  allocatable_memory=$(oc get node "$node_name" -o jsonpath='{.status.allocatable.memory}')
+  capacity_memory=$(oc get node "$node_name" -o jsonpath='{.status.capacity.memory}')
+  memory_line=$(oc describe node "$node_name" | awk '/Allocated resources:/,/Events:/' | grep '^  memory')
+  memory_requests=$(echo "$memory_line" | awk '{print $2}')
+  memory_limits=$(echo "$memory_line" | awk '{print $4}')
+
+  if [[ "$allocatable_memory" == *Ki ]]; then
+    alloc_mem_k=${allocatable_memory%Ki}
+    alloc_mem_m=$((alloc_mem_k / 1024))
+  elif [[ "$allocatable_memory" == *Mi ]]; then
+    alloc_mem_m=${allocatable_memory%Mi}
+  elif [[ "$allocatable_memory" == *Gi ]]; then
+    alloc_mem_m=$((allocatable_memory%Gi * 1024))
+  fi
+
+  req_mem_m=${memory_requests%Mi}
+  diff_mem_m=$((alloc_mem_m - req_mem_m))
+  diff_memory="${diff_mem_m}Mi"
+
+  printf "%-1s %-38s %-1s %-28s %-1s %-28s %-1s %-28s %-1s %-28s %-1s %-28s %-1s %-28s %-1s %-28s %-1s %-28s %-1s %-28s %-1s\n" "|" "$node_name" "|" "$allocatable_cpu" "|" "$capacity_cpu" "|" "$cpu_requests" "|" "$cpu_limits" "|" "$diff_cpu" "|" "$allocatable_memory" "|" "$capacity_memory" "|" "$memory_requests" "|" "$memory_limits" "|" "$diff_memory" "|"
+done
+
+printf -- "|----------------------------------------|------------------------------|------------------------------|------------------------------|------------------------------|------------------------------|------------------------------|------------------------------|------------------------------|------------------------------|------------------------------|\n"
